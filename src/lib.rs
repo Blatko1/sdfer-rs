3da10ae0965180a7166d10aa0ef45f54ce70f4c8
Blatko1 <kings.of.funny.gemplays@gmail.com>
2022-05-05 20:15:07 +0200

    Start

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+use glyph_brush_layout::{SectionGeometry, Layout};
+
+pub struct Section {
+
+}
+
+impl Section {
+    pub fn new() -> Self {
+        let fonts = 
+        let layout = Layout::default()
+
+        Self {
+
+        }
+    }
+}
